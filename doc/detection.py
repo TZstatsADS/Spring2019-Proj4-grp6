f651970c95719b2a1584df72b3bc73174f39bfc3
HyunBin Yoo <hy2506@columbia.edu>
2019-04-09 17:37:16 -0400

    feat: filtered files that do not have the same line numbers, and filtered the lines that do not have the same word counts in the line

--- doc/detection.py
+++ doc/detection.py
@@ -13,19 +13,57 @@ from collections import defaultdict
 def parseGroundTruth():
         
     truth_dict = defaultdict(int)
-    truth_count = 0
+
+    truth_counts = 0
     ground_tokens = []
     # create a list of all .txt files
-    truth_files = glob.glob('../data/ground_truth/*.txt')
+    truth_files_list = glob.glob('../data/ground_truth/*.txt')
     # reading the ground truth file
-    for file in truth_files:
+    for file in truth_files_list:
         with open(file) as fd:
             for line in fd:
                 each_line = line.strip().split()
                 for word in each_line:
                     ground_tokens.append(word)
-    print(len(ground_tokens))
-    print(ground_tokens[:20])
+                    truth_counts += 1
+
+
+    
+    test_files_list = glob.glob('../data/tesseract/*.txt')
+
+    # only taking the ones that have the same number of lines in the file
+
+    truth_files = []
+    test_files = []
+    file_counts = 0 # store number of files (test and truth have same length)
+
+    for truth, test in zip(truth_files_list, test_files_list):
+        truth_length = len(open(truth).readlines())    
+        test_length = len(open(test).readlines())
+        if truth_length == test_length:
+            file_counts += 1
+            truth_files.append(truth)
+            test_files.append(test)
+
+    # only taking lines that have the same number of words
+
+    truth_words = []
+    test_words = []
+    actual_counts = 0 # actual counts of numbers of words after filtering
+
+    for truth, test in zip(truth_files, test_words):
+        with open(truth) as fd_truth:
+            with open(test) as fd_test:
+                for truth_line, test_line in zip(fd_truth, fd_test):
+                    tmp_truth = truth_line.strip().split()
+                    tmp_test = test_line.strip().split()
+                    if len(tmp_truth) == len(tmp_test):
+                        for truth_word, test_word in zip(tmp_truth, tmp_test):
+                            actual_counts += 1
+                            truth_words.append(truth_word)
+                            test_words.append(test_word)
+
+
 
         
     
